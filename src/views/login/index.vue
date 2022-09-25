1c83d1bff8c856be53192d5b8ef609adb19ef065
Xiuer <Xiuer@example.com>
2022-09-25 16:25:43 +0800

    feature/login页面合在master

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,19 +2,22 @@
   <div class="login-container">
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
 
-      <div class="title-container">
-        <h3 class="title">Login Form</h3>
+      <div class="logo-pic">
+        <img
+          style="width: 96px; height: 96px"
+          src="@/assets/images/logo.png"
+          fit="cover"
+        >
       </div>
 
-      <el-form-item prop="username">
-        <span class="svg-container">
-          <svg-icon icon-class="user" />
-        </span>
+      <el-form-item prop="loginName">
+        <span class="svg-container el-icon-mobile" />
+
         <el-input
-          ref="username"
-          v-model="loginForm.username"
-          placeholder="Username"
-          name="username"
+          ref="loginName"
+          v-model="loginForm.loginName"
+          placeholder="请输入账号"
+          name="loginName"
           type="text"
           tabindex="1"
           auto-complete="on"
@@ -22,15 +25,13 @@
       </el-form-item>
 
       <el-form-item prop="password">
-        <span class="svg-container">
-          <svg-icon icon-class="password" />
-        </span>
+        <span class="svg-container el-icon-lock" />
         <el-input
           :key="passwordType"
           ref="password"
           v-model="loginForm.password"
           :type="passwordType"
-          placeholder="Password"
+          placeholder="请输入密码"
           name="password"
           tabindex="2"
           auto-complete="on"
@@ -41,58 +42,83 @@
         </span>
       </el-form-item>
 
-      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">Login</el-button>
+      <el-form-item class="code-item" prop="code">
+        <el-row>
+          <el-col :span="17"><div class="grid-content bg-purple" />
+            <span class="svg-container el-icon-lock" />
+            <el-input
+              ref="code"
+              v-model="loginForm.code"
+              placeholder="请输入验证码"
+              name="code"
+              type="text"
+              tabindex="3"
+              auto-complete="on"
+            /></el-col>
+          <el-col :span="7"><div class="grid-content bg-purple-light" />
+            <div class="code-image" @click="updateCode">
+              <img :src="codeUrl" alt="">
+            </div>
+          </el-col>
+        </el-row>
+      </el-form-item>
 
-      <div class="tips">
-        <span style="margin-right:20px;">username: admin</span>
-        <span> password: any</span>
-      </div>
-
+      <el-button class="loginBtn" :loading="loading" type="primary" @click.native.prevent="handleLogin">登录</el-button>
     </el-form>
+
   </div>
 </template>
 
 <script>
-import { validUsername } from '@/utils/validate'
-
+import { validloginName } from '@/utils/validate'
+import { updateCodeAPI } from '@/api'
 export default {
   name: 'Login',
   data() {
-    const validateUsername = (rule, value, callback) => {
-      if (!validUsername(value)) {
-        callback(new Error('Please enter the correct user name'))
+    const validateloginName = (rule, value, callback) => {
+      if (!validloginName(value)) {
+        callback(new Error('请输入账号'))
       } else {
         callback()
       }
     }
     const validatePassword = (rule, value, callback) => {
-      if (value.length < 6) {
-        callback(new Error('The password can not be less than 6 digits'))
+      if (value.length < 5) {
+        callback(new Error('请输入密码'))
+      } else {
+        callback()
+      }
+    }
+    const validateCode = (rule, value, callback) => {
+      if (value.length < 4) {
+        callback(new Error('请输入正确的验证码'))
       } else {
         callback()
       }
     }
     return {
       loginForm: {
-        username: 'admin',
-        password: '111111'
+        loginName: 'admin',
+        password: 'admin',
+        code: '',
+        clientToken: '',
+        loginType: 0
       },
       loginRules: {
-        username: [{ required: true, trigger: 'blur', validator: validateUsername }],
-        password: [{ required: true, trigger: 'blur', validator: validatePassword }]
+        loginName: [{ required: true, trigger: 'blur', validator: validateloginName }],
+        password: [{ required: true, trigger: 'blur', validator: validatePassword }],
+        code: [{ required: true, trigger: 'blur', validator: validateCode }]
       },
       loading: false,
       passwordType: 'password',
-      redirect: undefined
+      codeUrl: ''
+
     }
   },
-  watch: {
-    $route: {
-      handler: function(route) {
-        this.redirect = route.query && route.query.redirect
-      },
-      immediate: true
-    }
+  computed: { },
+  watch: {},
+  created() {
+    this.updateCode()
   },
   methods: {
     showPwd() {
@@ -105,22 +131,23 @@ export default {
         this.$refs.password.focus()
       })
     },
-    handleLogin() {
-      this.$refs.loginForm.validate(valid => {
-        if (valid) {
-          this.loading = true
-          this.$store.dispatch('user/login', this.loginForm).then(() => {
-            this.$router.push({ path: this.redirect || '/' })
-            this.loading = false
-          }).catch(() => {
-            this.loading = false
-          })
-        } else {
-          console.log('error submit!!')
-          return false
-        }
-      })
+    async updateCode() {
+      const clientToken = Math.random().toString()
+      this.loginForm.clientToken = clientToken
+      const res = await updateCodeAPI(clientToken)
+      this.codeUrl = res.config.url
+    },
+    async handleLogin() {
+      try {
+        await this.$refs.loginForm.validate()
+        this.loading = true
+        await this.$store.dispatch('user/LOGIN_ACTION', this.loginForm)
+        if (!this.$store.state.user.token) { this.$message.error(this.$store.state.user.data.msg) } else { this.$router.push('/') }
+      } finally {
+        this.loading = false
+      }
     }
+
   }
 }
 </script>
@@ -129,9 +156,9 @@ export default {
 /* 修复input 背景不协调 和光标变色 */
 /* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
 
-$bg:#283443;
-$light_gray:#fff;
-$cursor: #fff;
+$bg:#fff;
+$light_gray:#999;
+$cursor: #999;
 
 @supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
   .login-container .el-input input {
@@ -147,6 +174,7 @@ $cursor: #fff;
     width: 85%;
 
     input {
+      height: 47px;
       background: transparent;
       border: 0px;
       -webkit-appearance: none;
@@ -164,11 +192,42 @@ $cursor: #fff;
   }
 
   .el-form-item {
-    border: 1px solid rgba(255, 255, 255, 0.1);
-    background: rgba(0, 0, 0, 0.1);
-    border-radius: 5px;
+    width: 100%;
+    height: 52px;
+    margin-bottom: 24px;
+    background: #fff;
+    border: 1px solid #e2e2e2;
+    border-radius: 4px;
     color: #454545;
   }
+  .el-form-item__error {
+    color: #f56c6c;
+    font-size: 12px;
+    line-height: 1;
+    padding-top: 4px;
+    position: absolute;
+    top: 110%;
+    left: 0;
+}
+  .code-item{
+    .code-image{
+      width: 130px;
+      height:50px;
+      // background-color: lawngreen;
+    }
+  }
+
+  .loginBtn{
+    width: 100%;
+    height: 52px;
+    margin-bottom:30px;
+    background: linear-gradient(262deg,#2e50e1,#6878f0);
+    opacity: .91;
+    border-radius: 8px;
+    color: #fff;
+    text-shadow: 0 7px 22px #cfcfcf
+  }
+
 }
 </style>
 
@@ -178,29 +237,35 @@ $dark_gray:#889aa4;
 $light_gray:#eee;
 
 .login-container {
+  position: relative;
   min-height: 100%;
   width: 100%;
-  background-color: $bg;
-  overflow: hidden;
+    height: 100%;
+    width: 100%;
+    overflow: hidden;
+    background-image: url(~@/assets/images/background.png);
+    background-repeat: no-repeat;
+    background-size: cover;
 
   .login-form {
-    position: relative;
-    width: 520px;
-    max-width: 100%;
-    padding: 160px 35px 0;
-    margin: 0 auto;
-    overflow: hidden;
-  }
-
-  .tips {
-    font-size: 14px;
-    color: #fff;
-    margin-bottom: 10px;
-
-    span {
-      &:first-of-type {
-        margin-right: 16px;
-      }
+    position: absolute;
+    width: 518px;
+    height: 388px;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+    padding: 76px 35px 0;
+    background: #fff;
+    -webkit-box-shadow: 0 3px 70px 0 rgb(30 111 72 / 35%);
+    box-shadow: 0 3px 70px 0 rgb(30 111 72 / 35%);
+    border-radius: 10px;
+    .logo-pic{
+      position: absolute;
+    width: 96px;
+    height: 96px;
+    top: -46px;
+    left: 50%;
+    margin-left: -48px;
     }
   }
 
